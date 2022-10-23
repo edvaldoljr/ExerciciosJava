5ed17593bb69b73901d61facc4ba45e0ed55445e
edvaldojr <edvaldoluizj@gmail.com>
2022-10-23 11:01:49 -0300

    Exercicio 18

---
+++ src/exercicios/Exercicio18.java
@@ -0,0 +1,26 @@
+package exercicios;
+
+/**
+ * Imprima todos os números de 150 a 300.
+ */
+public class Exercicio18 {
+
+    public static void main(String[] args) {
+
+        int numero;
+        int i = 150;
+
+        for ( ; i <= 300 ; i++) {
+
+            System.out.println(i);
+        }
+
+        System.out.println("=================");
+        
+        while (i <= 300 ) {
+
+            System.out.println(i);
+            i++;
+        }
+    }
+}
